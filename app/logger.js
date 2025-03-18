52ca6eb3d91f62f350ab6e7d07f14fc3d47176cb
pmsu2007 <pmsu2007@naver.com>
2025-03-18 23:30:56 +0900

    feat: 로깅을 위한 logger와 로깅 메시지 생성

---
+++ app/logger.js
@@ -0,0 +1,25 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info", // error > warn > info
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    }),
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "app.log" }),
+  ],
+});
+
+const formatMessage = (message, data = {}) => {
+  return Object.keys(data).reduce(
+    (msg, key) => msg.replace(`{${key}}`, data[key]),
+    message,
+  );
+};
+
+module.exports = { logger, formatMessage };

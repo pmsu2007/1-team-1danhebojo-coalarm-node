b38925de5122ff394740564d6fe149174078aaff
pmsu2007 <pmsu2007@naver.com>
2025-03-18 16:38:57 +0900

    fix: ESlint 설정 파일을 CJS 방식으로 수정

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,15 +1,15 @@
-export default [
-    {
-        files: ["app/**/*.js"],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-        },
-        rules: {
-            "semi": ["error", "always"],
-            "quotes": ["error", "single"],
-            "no-unused-vars": "warn",
-            "no-console": "off"
-        },
+module.exports = [
+  {
+    files: ["app/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    },
+  },
 ];

acdcd3e7f8315a0f8a0bcf5634b7d9f62129e781
pmsu2007 <pmsu2007@naver.com>
2025-03-31 18:53:38 +0900

    fix: lint 설정을 수정

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -6,8 +6,6 @@ module.exports = [
       sourceType: "module",
     },
     rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"],
       "no-unused-vars": "warn",
       "no-console": "off"
     },

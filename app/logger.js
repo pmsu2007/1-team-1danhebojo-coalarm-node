66b54aece289a84245d93e816b8c64946d3da871
pmsu2007 <pmsu2007@naver.com>
2025-03-31 18:44:30 +0900

    fix: 콘솔 로깅과 파일 로깅 설정 분리

--- app/logger.js
+++ app/logger.js
@@ -1,17 +1,25 @@
-const winston = require('winston');
+const winston = require("winston");
+
+const consoleFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+  }),
+);
+
+const fileFormat = winston.format.combine(
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+  }),
+);
 
 const logger = winston.createLogger({
-  level: 'info', // error > warn > info
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    }),
-  ),
+  level: "info",
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'app.log' }),
+    new winston.transports.Console({ format: consoleFormat }),
+    new winston.transports.File({ filename: "app.log", format: fileFormat }),
   ],
 });
 

a8fca05645d992431a51b9fe3b03a609588150e9
pmsu2007 <pmsu2007@naver.com>
2025-03-31 18:49:42 +0900

    feat: 로깅을 위한 메시지 추가

--- app/messages.js
+++ app/messages.js
@@ -1,19 +1,30 @@
 const messages = {
   app: {
-    start: '서버가 시작되었습니다.',
-    stop: '서버가 중지되었습니다.',
-    restart: '서버가 재시작되었습니다.',
-    connectTicker: '티커 데이터 웹소켓 연결에 성공했습니다: {app}',
-    connectTrade: '체결 데이터 웹소켓 연결에 성공했습니다: {app}',
+    start: "서버가 시작되었습니다.",
+    stop: "서버가 중지되었습니다.",
+    restart: "서버가 재시작되었습니다.",
+    connectTicker: "티커 데이터 웹소켓 연결에 성공했습니다: {app}",
+    connectTrade: "체결 데이터 웹소켓 연결에 성공했습니다: {app}",
   },
   db: {
-    connect: '데이터베이스에 성공적으로 연결되었습니다.',
+    connect: "데이터베이스에 성공적으로 연결되었습니다.",
   },
   error: {
-    failConnectDB: '데이터베이스 연결에 실패했습니다: {error}',
-    failInsertDB: '데이터베이스에 데이터를 저장하는 데애 실패했습니다: {error}',
-    failWatchTicker: '티커 데이터를 받아오는 데에 실패했습니다: {error}',
-    failWatchTrade: '체결 데이터를 받아오는 데에 실패했습니다: {error}',
+    failConnectDB: "데이터베이스 연결에 실패했습니다: {error}",
+    failReadCoin: "코인 데이터를 조회하는 데에 실패했습니다: {error}",
+    failInsertCoin: "코인 데이터를 저장하는 데에 실패했습니다: {error}",
+    failDeleteCoin: "코인 데이터를 삭제하는 데에 실패했습니다: {error}",
+    failUpdateCoin: "코인 데이터를 수정하는 데에 실패했습니다: {error}",
+    failInsertTicker: "티커 데이터를 저장하는 데에 실패했습니다: {error}",
+    failInsertTrade: "체결 내역 데이터를 저장하는 데에 실패했습니다: {error}",
+    failWatchTickerUpbit:
+      "[업비트] 티커 데이터를 받아오는 데에 실패했습니다: {error}",
+    failWatchTickerBinance:
+      "[바이낸스] 티커 데이터를 받아오는 데에 실패했습니다: {error}",
+    failWatchTradeUpbit:
+      "[업비트] 체결 데이터를 받아오는 데에 실패했습니다: {error}",
+    failWatchTradeBinance:
+      "[바이낸스] 체결 데이터를 받아오는 데에 실패했습니다: {error}",
   },
 };
 
